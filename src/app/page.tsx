cbb75e7577ed276fa96c4c1db65143c6d81657ca
tapi0623 <matsudakai0623@gmail.com>
2024-05-05 14:51:12 +0900

    Chore: Reconstruct NextUI enviroment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
-export default function Page() {
-  return <h1>Hello, tapiokai.net</h1>;
+import { Button } from "@nextui-org/button";
+
+export default function Home() {
+  return <Button>Hello, tapiokai.net</Button>;
 }
