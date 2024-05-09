1afcfa993b22135e06bfd531d88fa20a77416459
tapi0623 <matsudakai0623@gmail.com>
2024-05-09 22:50:34 +0900

    Fix: site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "tapiokai.net",
+	description: "tapiokai.net ゆる～く更新したりしなかったり",
 	navItems: [
 		{
 			label: "Home",
