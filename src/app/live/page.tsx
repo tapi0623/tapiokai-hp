095512ef8650e54b70eb038c6b2a81324c53a7c8
tapi0623 <matsudakai0623@gmail.com>
2024-06-28 17:39:00 +0900

    Feat: Embed owncast

---
+++ src/app/live/page.tsx
@@ -0,0 +1,12 @@
+export function Page() {
+  return (
+    <iframe
+      src="https://localhost:8080/embed/video"
+      title="Owncast"
+      height="350px"
+      width="550px"
+      referrerPolicy="origin"
+      allowFullScreen
+    ></iframe>
+  );
+}
