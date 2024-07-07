659f0780118028825d880b8f4854ba738dc7d2a1
tapi0623 <matsudakai0623@gmail.com>
2024-07-07 14:29:12 +0900

    Feat: migrate db

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `content` TEXT NOT NULL,
+    `thumbnail_path` TEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
